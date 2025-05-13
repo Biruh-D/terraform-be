dfee1412989e3fb98674c7bc157ef5929ced6cd3
Biruh-D <155105526+Biruh-D@users.noreply.github.com>
2025-05-12 22:41:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,51 @@
-/* third party packages */
-// cors error, raw node js
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const aws = require('aws-sdk');
-const User = require("./models/user");
-const Resource = require("./models/resource");
-const MyList = require("./models/mylist");
-const userRoutes = require("./routes/user");
-const resourceRoutes = require("./routes/resource");
-const sequelize = require("./util/database");
-
-
-//
+// Load env first
 require('dotenv').config();
-const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST, // Replace with your RDS endpoint
-  user: process.env.DB_USER,  // Replace with your DB username
-  password: process.env.DB_PASS, // Replace with your DB password
-  database: process.env.DB_NAME,  // Replace with your DB name
-  port: 3306  // Default MySQL port
-});
+/* third-party packages */
+const express    = require('express');
+const bodyParser = require('body-parser');
+const path       = require('path');
+const aws        = require('aws-sdk');
+const cors       = require('cors');
 
-// Connect to the database
-connection.connect(err => {
-  if (err) {
-    console.error('Error connecting: ' + err.stack);
-    return;
-  }
-  console.log('Connected as id ' + connection.threadId);
-});
+// Configure AWS SDK
+aws.config.update({ region: process.env.AWS_REGION });
 
-module.exports = connection;
+// Database via Sequelize
+const sequelize = require('./util/database');
+// If you still need raw connections, configure here
+// const connection = require('./util/mysql');
 
+// Models & Routes
+const User     = require('./models/user');
+const Resource = require('./models/resource');
+const MyList   = require('./models/mylist');
+const userRoutes     = require('./routes/user');
+const resourceRoutes = require('./routes/resource');
 
-// const verifyToken = require("./middlewares/authentication");
-
-// import
 const app = express();
-app.set("trust proxy", true);
-const cors = require('cors');
+app.set('trust proxy', true);
 app.use(cors({
-    origin: 'https://resourcexchange.net', // Or use '*' for all origins (but this is not recommended in production)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Allow cookies to be sent across domains if needed
+  origin: 'https://resourcexchange.net',
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true,
 }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true, limit: 1024 * 1024 }));
-
-// app.use(verifyToken())
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
 app.use(userRoutes);
 app.use(resourceRoutes);
 
-User.hasMany(Resource, { foreignKey: "userId" });
-Resource.belongsTo(User, { foreignKey: "userId" });
+// Associations
+User.hasMany(Resource,{ foreignKey:'userId' });
+Resource.belongsTo(User,{ foreignKey:'userId' });
+User.hasMany(MyList,{ foreignKey:'userId' });
+MyList.belongsTo(User,{ foreignKey:'userId' });
+Resource.hasMany(MyList,{ foreignKey:'resourceId' });
+MyList.belongsTo(Resource,{ foreignKey:'resourceId' });
 
-User.hasMany(MyList, { foreignKey: "userId" });
-MyList.belongsTo(User, { foreignKey: "userId" });
-
-Resource.hasMany(MyList, { foreignKey: "resourceId" });
-MyList.belongsTo(Resource, { foreignKey: "resourceId" });
-
-sequelize
-  .sync()
-  .then((data) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// Start server after sync
+sequelize.sync()
+  .then(() => app.listen(3000, () => console.log('Server listening on port 3000')))
+  .catch(err => console.error('Sync error:', err));
